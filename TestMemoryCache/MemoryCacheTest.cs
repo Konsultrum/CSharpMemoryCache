14a2d6c23e2e186b2776d04a3c15436c376c795b
Tahir Alvi <engtahiralvi@gmail.com>
2020-09-04 00:51:29 +0500

    Initial Commit
    Initial Commit

---
+++ TestMemoryCache/MemoryCacheTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace TestMemoryCache.Test
+{
+    public class MemoryCacheTest
+    {
+        private MemoryCache cache;
+        private readonly CacheItemPolicy cacheItemPolicy;
+
+        public MemoryCacheTest()
+        {
+            // initialize the MemoryCache and CacheItemPolicy at constructor level
+            cache = new MemoryCache("Test");
+            cacheItemPolicy = new CacheItemPolicy
+            {
+                AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(600.0)
+            };            
+        }
+
+        /// <summary>
+        /// This method is used to check the incoming input and act accordingly
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string CheckInput(string input)
+        {
+            string result = string.Empty; 
+            
+            // check for empty or null input
+            if (string.IsNullOrEmpty(input))
+            {
+                return "Please use a valid input";
+            }
+
+
+            if (input.Length > 0) // if input has some value
+            {
+                if (input.Contains("|")) // check if the input contain pipe (|)
+                {
+                    var arrInput = input.Split('|');
+                    if (arrInput[0].ToString() == "Add")
+                    {
+                        result = AddIntoCache(arrInput[1].ToString());
+                    }
+                    else if (arrInput[0].ToString() == "Get")
+                    {
+                        result = GetFromCache(arrInput[1].ToString());
+                    }
+                    else if (arrInput[0].ToString() == "Has")
+                    {
+                        result = CheckCache(arrInput[1].ToString());
+                    }
+                    else if (arrInput[0].ToString() == "Remove")
+                    {
+                        result = RemoveFromCache(arrInput[1].ToString());
+                    }
+                }
+                else // simple input without pipe (|)
+                {
+                    result = ResetCache();
+                }
+                
+            }
+            return result;
+        }
+
+        private string RemoveFromCache(string param)
+        {
+            return cache.Remove(param).ToString().Length > 0 ? "True" : "False";
+        }
+
+        private string ResetCache()
+        {
+            var countItems = cache.GetCount().ToString();
+            cache.Dispose(); // Reset the Cache object
+            return countItems;
+        }
+
+        private string AddIntoCache(string param)
+        {
+            var cacheItem = new CacheItem(param, param);
+
+            return Convert.ToString(cache.Add(cacheItem, cacheItemPolicy));
+        }
+
+        private string GetFromCache(string param)
+        {
+            if (cache.Contains(param))
+            {
+                return cache.Get(param).ToString();
+            }
+            else
+            {
+                return "NULL";
+            }
+            
+        }
+
+        private string CheckCache(string param)
+        {
+            return cache.Contains(param).ToString();
+        }
+    }
+}
---
+++ TestMemoryCache/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using TestMemoryCache.Test;
+
+namespace TestMemoryCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemoryCacheTest memoryCacheTest = new MemoryCacheTest();
+
+            // Test 1
+            //Console.WriteLine(memoryCacheTest.CheckInput("Add|Dog"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Get|Dog"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Remove|Dog"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Has|Dog"));
+
+            // Test 2
+            //Console.WriteLine(memoryCacheTest.CheckInput("Get|English"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Add|English"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Add|German"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Add|Mandarin"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Add|Malay"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Get|German"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Get|Mandarin"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Has|French"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Add|French"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Has|French"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Reset"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Has|English"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Has|German"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Has|Mandarin"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Has|Malay"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Has|French"));
+
+            //Test 3
+            //Console.WriteLine(memoryCacheTest.CheckInput("Add|A"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Add|B"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Add|C"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Add|D"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Get|A"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Get|B"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Get|1"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Get|2"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Add|1"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Add|2"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Get|2"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Remove|A"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Remove|B"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Remove|C"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Has|A"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Get|A"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Add|A"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Has|A"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Get|A"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Reset"));
+            //Console.WriteLine(memoryCacheTest.CheckInput("Get|C"));
+
+            Console.ReadLine();
+        }
+    }
+}
